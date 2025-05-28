9a1b45e1d23de72f1ab9601caf4060c15fd7f26e
Βάιος Ριζούλης <rizoulisvagios@Mac.home>
2025-05-28 23:15:53 +0300

    changes in calendar

--- Front-End/eventify/src/components/Calendar.jsx
+++ Front-End/eventify/src/components/Calendar.jsx
@@ -2,6 +2,10 @@ import {useState} from "react";
 import "../App.css"; 
 
 const Calendar = () => {
+
+
+
+ 
   const months = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
@@ -11,40 +15,53 @@ const Calendar = () => {
 
   const currYear = date.getFullYear();
   const currMonth = date.getMonth();
+  const currDay = date.getDate();
 
   const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
   const lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
   const lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay();
   const lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
 
+  const [events, setEvents] = useState([{
+    date: new Date(currYear,currMonth,currDay+1) 
+  }
+]);
+
+  // setEvents(prev => [...prev, { 
+  //  date: ""
+  // }
+  // ]);
+
   const generateCalendarDays = () => {
     
     let days = [
     ];
 
+    let flag = true;
+
     for (let i = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1; i > 0; i--) {
-      days.push(<li key={`prev-${i}`} className="inactive">{lastDateOfLastMonth - i + 1}</li>);
+      days.push(<li className="inactive">{lastDateOfLastMonth - i + 1}</li>);
     }
 
     for (let i = 1; i <= lastDateOfMonth; i++) {
-      const isToday =
-        i === new Date().getDate() &&
-        currMonth === new Date().getMonth() &&
-        currYear === new Date().getFullYear();
 
       days.push(
-        <li key={`curr-${i}`} className={isToday ? "active" : ""}>
+        <li className={(events.some(event =>
+          event.date.getFullYear() === currYear &&
+          event.date.getMonth() === currMonth &&
+          event.date.getDate() === i)) ? "active" : ""}>
           {i}
         </li>
       );
+
+    }
+    
+    let k = lastDayOfMonth === 0 ? 0 : 7 - lastDayOfMonth;
+    for (let i = 1; i <= k; i++) {
+      days.push(<li className="inactive">{i}</li>);
     }
 
-    
-    for (let i = lastDayOfMonth === 0 ? 0 : 7 - lastDayOfMonth; i > 0; i--) {
-    
-        days.push(<li key={`next-${i}`} className="inactive">{i}</li>);
-    }
-
+        
     return days;
   };
 
