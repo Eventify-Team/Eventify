ff4232dd74c0342f90f388a6fb1e9f3b2b5332f4
KyriakosSousloglou <sousloglou1914@gmail.com>
2025-05-29 11:13:27 +0300

    Test

--- Back-End/eventify/src/main/java/com/evetify/eventify/WebConfig.java
+++ Back-End/eventify/src/main/java/com/evetify/eventify/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    //O Vagios einai magos
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
